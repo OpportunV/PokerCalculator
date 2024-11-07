02c405a7ea35535121b412508129af6112d5be01
OpportunV <RsTGear@gmail.com>
2024-11-08 00:48:11 +0300

    Added ps script

---
+++ .github/scripts/check-coverage.ps1
@@ -0,0 +1,12 @@
+﻿$coveragePercentage = Select-String -Path './coverage/lcov.info' -Pattern '"lineCoverage":\K[0-9.]*' | ForEach-Object { $_.Matches.Groups[0].Value }
+
+Write-Host "Coverage Percentage: $coveragePercentage"
+
+$coverageThreshold = $env:COVERAGE_THRESHOLD
+
+if ([double]$coveragePercentage -lt [double]$coverageThreshold) {
+    Write-Host "Coverage percentage is below threshold ($coverageThreshold%)."
+    exit 1
+} else {
+    Write-Host "Coverage meets the threshold."
+}
